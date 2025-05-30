c2ea9259dc93d87304483e0d7c9229d7cea1af70
MohamedGasser15 <madagasser15@gmail.com>
2025-05-30 19:22:59 +0300

    feat: add AutoMapper configuration to API project
    Summary:
    - Created a MappingConfig class to centralize AutoMapper profiles.
    - Set up CreateMap configurations between core models and DTOs.
    - Registered AutoMapper in Program.cs for dependency injection.
    - Prepares the API for clean and scalable object mapping.

---
+++ EduLab_API/MappingConfig.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace EduLab_API
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+        }
+    }
+}
--- EduLab_API/Program.cs
+++ EduLab_API/Program.cs
@@ -1,3 +1,5 @@
+using EduLab_API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
