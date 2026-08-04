370cccdbcd3d48df938ee3f6fcb139aaa6f8f4f8
Mohamedd Gasser <madagasser15@gmail.com>
2026-08-04 22:29:08 +0300

    feat: add section-level free preview UI with video preview modal in Details page

--- apps/web/EduLab_MVC/Models/DTOs/Course/SectionCreateDTO.cs
+++ apps/web/EduLab_MVC/Models/DTOs/Course/SectionCreateDTO.cs
@@ -4,5 +4,6 @@ namespace EduLab_MVC.Models.DTOs.Course
     {
         public string Title { get; set; }
         public int CourseId { get; set; }
+        public bool IsFreePreview { get; set; }
     }
 }
--- apps/web/EduLab_MVC/Models/DTOs/Course/SectionUpdateDTO.cs
+++ apps/web/EduLab_MVC/Models/DTOs/Course/SectionUpdateDTO.cs
@@ -4,5 +4,6 @@ namespace EduLab_MVC.Models.DTOs.Course
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public bool IsFreePreview { get; set; }
     }
 }
