24d24da9009158cb6ea57e8099143e603605835d
Mohamedd Gasser <madagasser15@gmail.com>
2026-08-07 22:54:11 +0300

    feat: add operation type badges and filters to admin and instructor history views

--- apps/web/EduLab_MVC/Models/DTOs/History/HistoryDTO.cs
+++ apps/web/EduLab_MVC/Models/DTOs/History/HistoryDTO.cs
@@ -2,20 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace EduLab_MVC.Models.DTOs.History
 {
     public class HistoryDTO
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
 
+        [JsonPropertyName("userName")]
         public string UserName { get; set; } = string.Empty;
+
+        [JsonPropertyName("profileImageUrl")]
         public string? ProfileImageUrl { get; set; }
+
+        [JsonPropertyName("operation")]
         public string Operation { get; set; } = string.Empty;
 
+        [JsonPropertyName("operationKeyId")]
+        public int? OperationKeyId { get; set; }
+
+        [JsonPropertyName("operationKeyName")]
+        public string? OperationKeyName { get; set; }
+
+        [JsonPropertyName("date")]
         public DateOnly Date { get; set; }
 
+        [JsonPropertyName("time")]
         public TimeOnly Time { get; set; }
+
+        public string BadgeClass => OperationKeyId switch
+        {
+            1 => "op-delete",   // Delete
+            10 => "op-delete",  // Reject
+            7 => "op-delete",   // Lock
+            2 => "op-create",   // Create
+            9 => "op-create",   // Approve
+            8 => "op-create",   // Unlock
+            6 => "op-create",   // Publish
+            4 => "op-update",   // Edit
+            _ => "op-default"
+        };
     }
 }
