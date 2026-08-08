748d4e176ec2db38dacef3d2ad52594f0a6956f3
Mohamedd Gasser <madagasser15@gmail.com>
2026-08-08 05:40:20 +0300

    feat: populate instructor reviews page with real ratings data

---
+++ apps/web/EduLab_MVC/Models/DTOs/Instructor/InstructorRatingsDTO.cs
@@ -0,0 +1,76 @@
+using Newtonsoft.Json;
+
+namespace EduLab_MVC.Models.DTOs.Instructor
+{
+    public class InstructorRatingsDTO
+    {
+        [JsonProperty("stats")]
+        public InstructorRatingsStatsDTO Stats { get; set; } = new();
+
+        [JsonProperty("courses")]
+        public List<InstructorRatingCourseDTO> Courses { get; set; } = new();
+
+        [JsonProperty("reviews")]
+        public List<InstructorRatingItemDTO> Reviews { get; set; } = new();
+    }
+
+    public class InstructorRatingsStatsDTO
+    {
+        [JsonProperty("totalReviews")]
+        public int TotalReviews { get; set; }
+
+        [JsonProperty("averageRating")]
+        public double AverageRating { get; set; }
+
+        [JsonProperty("thisMonthReviews")]
+        public int ThisMonthReviews { get; set; }
+
+        [JsonProperty("reviewedCourses")]
+        public int ReviewedCourses { get; set; }
+
+        [JsonProperty("distribution")]
+        public Dictionary<int, int> Distribution { get; set; } = new()
+        {
+            { 1, 0 }, { 2, 0 }, { 3, 0 }, { 4, 0 }, { 5, 0 }
+        };
+    }
+
+    public class InstructorRatingCourseDTO
+    {
+        [JsonProperty("courseId")]
+        public int CourseId { get; set; }
+
+        [JsonProperty("courseName")]
+        public string CourseName { get; set; }
+    }
+
+    public class InstructorRatingItemDTO
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("courseId")]
+        public int CourseId { get; set; }
+
+        [JsonProperty("courseName")]
+        public string CourseName { get; set; }
+
+        [JsonProperty("studentName")]
+        public string StudentName { get; set; }
+
+        [JsonProperty("studentAvatar")]
+        public string? StudentAvatar { get; set; }
+
+        [JsonProperty("rating")]
+        public int Rating { get; set; }
+
+        [JsonProperty("comment")]
+        public string Comment { get; set; }
+
+        [JsonProperty("createdAt")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty("timeAgo")]
+        public string TimeAgo { get; set; }
+    }
+}
