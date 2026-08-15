ad5c0715acd304155a1d2854e1d463772ec2f27a
Mohamedd Gasser <madagasser15@gmail.com>
2026-08-15 20:06:55 +0300

    refactor: use dashboard data in instructor reports page

--- apps/web/EduLab_MVC/Areas/Instructor/Controllers/ReportsController.cs
+++ apps/web/EduLab_MVC/Areas/Instructor/Controllers/ReportsController.cs
@@ -1,6 +1,8 @@
 using EduLab_MVC.Common;
+using EduLab_MVC.Services.ServiceInterfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace EduLab_MVC.Areas.Instructor.Controllers
 {
@@ -8,8 +10,22 @@ namespace EduLab_MVC.Areas.Instructor.Controllers
     [Authorize(Roles = SD.Instructor)]
     public class ReportsController : Controller
     {
-        public IActionResult Index()
+        private readonly IDashboardService _dashboardService;
+
+        public ReportsController(IDashboardService dashboardService)
         {
+            _dashboardService = dashboardService;
+        }
+
+        public async Task<IActionResult> Index(string period = "month")
+        {
+            var dashboard = await _dashboardService.GetInstructorDashboardAsync();
+            var revenue = await _dashboardService.GetInstructorRevenueAsync(period);
+
+            ViewBag.Dashboard = dashboard;
+            ViewBag.Revenue = revenue;
+            ViewBag.SelectedPeriod = string.IsNullOrWhiteSpace(period) ? "month" : period;
+
             return View();
         }
     }
